9f811a80d273a7e19f8eda6ba1fc4e8d6c6de292
sekaiya <sekai.kobayashi@gmail.com>
2013-10-07 21:08:57 +0900

    make doc by YARD

--- lib/gem_sample_pruby.rb
+++ lib/gem_sample_pruby.rb
@@ -1,6 +1,9 @@
 require "gem_sample_pruby/version"
 
+# ==let's omikuji
 module GemSamplePruby
+	# @param[nil] your feeling!
+	# @return [string]today's fortune
 	def self.omikuji
 		results = ["daikichi","kichi","hazure"]
 		p results.sample
